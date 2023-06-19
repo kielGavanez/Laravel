9619c8bda345fafbb472db2dad3ce6ef269a3b66
kielGavanez <130537228+kielGavanez@users.noreply.github.com>
2023-06-19 16:27:42 +0800

    new laravel practice

--- resources/views/mainpage.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('template.app')
-
-@section('content')
-<h1>Welcome to Main Page</h1>
-@endsection
--- resources/views/welcome.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('template.app')
-
-@section('content')
-<h1>Welcome to Welcome Page</h1>
-@endsection
